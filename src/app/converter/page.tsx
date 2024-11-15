98ac68e59a86c699c5ec13d2dc0917399c6ad879
Zach Cohn <zcohn@wustl.edu>
2024-11-15 14:37:32 -0600

    removed nextui

--- src/app/converter/page.tsx
+++ src/app/converter/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { Autocomplete, AutocompleteItem } from "@nextui-org/react";
 import { convertFile } from "@/app/actions/convertFile";
 
 export default function Converter() {
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,6 @@
-import {NextUIProvider} from '@nextui-org/react'
 
 export function Providers({children}: { children: React.ReactNode }) {
   return (
-    <NextUIProvider>
       {children}
-    </NextUIProvider>
   )
 }
